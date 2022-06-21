318fd45f1c3d0d211b177a1eeedb10770d162794
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2022-06-21 10:13:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #title:         JOD-ALPHA-Dependency-Installer
 
+
+apt install python3 -y
+apt install python3-pip -y
+apt install git -y
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/waybackurls@latest
