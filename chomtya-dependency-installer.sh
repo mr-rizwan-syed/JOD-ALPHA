c4dd20b350659939b74639005b9797f2a776f9f2
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2022-09-14 15:02:49 +0530

    Update chomtya-dependency-installer.sh

--- chomtya-dependency-installer.sh
+++ chomtya-dependency-installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #title:         Chomtya-Dependency-Installer
 
-apt install nmap
-apt install xsltproc
-apt install csvkit
+apt install nmap -y
+apt install xsltproc -y
+apt install csvkit -y
 pip install xmlmerge
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
