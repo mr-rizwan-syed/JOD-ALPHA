4a0805ff7bc409f4eed9809942f90f266fc4d73a
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2022-09-14 14:08:03 +0530

    Update chomtya-dependency-installer.sh

--- chomtya-dependency-installer.sh
+++ chomtya-dependency-installer.sh
@@ -6,5 +6,6 @@ apt install xsltproc
 pip install xmlmerge
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 nuclei -update
 nuclei -ut
